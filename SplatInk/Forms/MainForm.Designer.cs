b579bf03020bfacee8323f7cc2837fd905eb6e62
Dan Banfield <dandbanfield@gmail.com>
2022-10-13 20:39:55 +0100

    Add application icon.

--- SplatInk/Forms/MainForm.Designer.cs
+++ SplatInk/Forms/MainForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.tcpGeckoConnectionGroupBox = new System.Windows.Forms.GroupBox();
             this.connectionStatusLabel = new System.Windows.Forms.Label();
             this.wiiUIPAddressTextBox = new System.Windows.Forms.TextBox();
@@ -128,6 +129,7 @@
             this.Controls.Add(this.changeInkColourButton);
             this.Controls.Add(this.tcpGeckoConnectionGroupBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
