6e7e8e6a52a773f81eaf7a060197c2e19819b465
Dan Banfield <dandbanfield@gmail.com>
2022-10-13 19:21:56 +0100

    Update project structure.

--- SplatInk/Forms/MainForm.Designer.cs
+++ SplatInk/Forms/MainForm.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace SplatInk
+﻿namespace SplatInk.Forms
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(370, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SplatInk";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
